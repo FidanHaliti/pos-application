26a4fad232698c8c813da1ef9fec8ec44cec2871
Fidan Haliti <fidan91haliti@gmail.com>
2023-07-31 02:38:40 +0200

    video 52

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,31 +1,64 @@
-const Categories = () => {
+import { PlusOutlined } from "@ant-design/icons";
+import "./style.css";
+import { useState } from "react";
+import { Button, Form, Input, Modal, message } from "antd";
+
+const Categories = ({ categories, setCategories }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    try {
+      fetch("http://localhost:5000/api/categories/add-category", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: { "Content-Type": "application/json; charset=UTF-8" },
+      });
+      message.success("The category has been successfully added.");
+      form.resetFields();
+      setCategories( [...categories, values])
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <ul className="flex md:flex-col gap-4 text-lg">
-      <li className="bg-green-700 text-white px-6 py-10 text-center cursor-pointer transition-all hover:bg-pink-700 min-w-[145px]">
-        <span>All</span>
+      {categories.map((item) => (
+        <li className="category-item" key={item._id}>
+          <span>{item.title}</span>
+        </li>
+      ))}
+
+      <li
+        className="category-item bg-purple-800 hover:opacity-90 "
+        onClick={() => setIsModalOpen(true)}
+      >
+        <PlusOutlined className="md:text-2xl flex" />
       </li>
-      <li className="bg-green-700 text-white px-6 py-10 text-center cursor-pointer transition-all hover:bg-pink-700 min-w-[145px]">
-        <span>All</span>
-      </li>
-      <li className="bg-green-700 text-white px-6 py-10 text-center cursor-pointer transition-all hover:bg-pink-700 min-w-[145px]">
-        <span>All</span>
-      </li>
-      <li className="bg-green-700 text-white px-6 py-10 text-center cursor-pointer transition-all hover:bg-pink-700 min-w-[145px]">
-        <span>All</span>
-      </li>
-      <li className="bg-green-700 text-white px-6 py-10 text-center cursor-pointer transition-all hover:bg-pink-700 min-w-[145px]">
-        <span>All</span>
-      </li>
-      <li className="bg-green-700 text-white px-6 py-10 text-center cursor-pointer transition-all hover:bg-pink-700 min-w-[145px]">
-        <span>All</span>
-      </li>
-      <li className="bg-green-700 text-white px-6 py-10 text-center cursor-pointer transition-all hover:bg-pink-700 min-w-[145px]">
-        <span>All</span>
-      </li>
-      <li className="bg-green-700 text-white px-6 py-10 text-center cursor-pointer transition-all hover:bg-pink-700 min-w-[145px]">
-        <span>All</span>
-      </li>
-     
+      <Modal
+        title="New Add Category"
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        footer={false}
+      >
+        <Form layout="vertical" onFinish={onFinish} form={form}>
+          <Form.Item
+            name="title"
+            label="Add Category :"
+            rules={[
+              { required: true, message: "empty category is not accepted!" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item className="flex justify-end mb-0">
+            <Button type="primary" htmlType="submit">
+              {" "}
+              Add
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </ul>
   );
 };
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,14 +2,38 @@ import CartTotals from "../components/cart/CartTotals";
 import Categories from "../components/categories/Categories";
 import Header from "../components/header/Header";
 import Products from "../components/products/Products";
+import { useState, useEffect } from "react";
 
 const HomePage = () => {
+  const [categories, setCategories] = useState([])
+
+useEffect(() => {
+  const getCategories = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/api/categories/get-all");
+      const data = await res.json();
+      console.log(data);
+      setCategories(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+getCategories()
+
+ 
+    
+  }
+, [])
+
+
+
   return (
     <div>
       <Header></Header>
       <div className="home flex justify-between px-6 gap-10 md:flex-row flex-col md:pb-0 pb-24 ">
         <div className="categories  overflow-auto max-h-[calc(100vh_-_103px)] md:pb-10">
-          <Categories />
+          <Categories categories={categories} setCategories={setCategories} />
         </div>
         <div className="products flex-[8] max-h-[calc(100vh_-_103px)]  overflow-y-auto">
           <Products />
